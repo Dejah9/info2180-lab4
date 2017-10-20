c3ab3b3ac5bd6f245659035c18e479f44ddefe24
Dejah9 <khadejahhylton@yahoo.com>
2017-10-20 18:53:02 -0500

    ex 4 complete. maze is restart-able

--- maze.js
+++ maze.js
@@ -2,40 +2,52 @@
     var gameStart = false;
     var lose = false;
 
-//Exercise 3
+//Exercise 4
 window.onload = function(){
     var boundary = document.querySelectorAll(".boundary");
-
 
     for (var i=0; i< boundary.length-1; i++){
         boundary[i].addEventListener("mouseover", lost);
     }
 
+    
     document.getElementById("end").addEventListener("mouseover", endgame);
+    document.getElementById("start").addEventListener("click", gameGo);
+
 }
 
     
 
 function lost(){
-    if (!gameStart){
+    if (gameStart){
+        console.log(this);
         var boundary = document.querySelectorAll(".boundary");
-    for (var i = 0; i < boundary.length-1; i++) {
-        boundary[i].className += " youlose";
-        gameStart = true;
-        document.getElementById("status").innerHTML="You Lose!";
+        for (var i = 0; i < boundary.length-1; i++) {
+            boundary[i].className += " youlose";
+            document.getElementById("status").innerHTML="You Lose!";
         }
+        lose = true;
     }
 }
  
 function endgame(){
-    if (!gameStart)
-        {
+    if(lose){
            document.getElementById("status").innerHTML="You Win!";
-        }
-    else
-        {
-            document.getElementById("status").innerHTML="You Lose!";
-        }
+    }
 }
 
 
+
+function gameGo(){
+    resetBoundary("boundary");
+    gameStart = true;
+    document.getElementById("status").innerHTML="PLAYING";
+}
+
+
+function resetBoundary(cssName){
+    var boundary = document.querySelectorAll(".boundary");
+    for (var i = 0; i < boundary.length-1; i++) {
+        boundary[i].className = cssName;
+    } 
+}
