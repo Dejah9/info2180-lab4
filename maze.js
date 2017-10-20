73b4ea26d6dcb6eb7325b49c855b7845d3988536
Dejah9 <khadejahhylton@yahoo.com>
2017-10-20 16:51:05 -0500

    Ex3 complete. message to player added

--- maze.js
+++ maze.js
@@ -2,16 +2,18 @@
     var gameStart = false;
     var lose = false;
 
-//Exercise 2
+//Exercise 3
 window.onload = function(){
     var boundary = document.querySelectorAll(".boundary");
 
 
     for (var i=0; i< boundary.length-1; i++){
         boundary[i].addEventListener("mouseover", lost);
-
     }
+
+    document.getElementById("end").addEventListener("mouseover", endgame);
 }
+
     
 
 function lost(){
@@ -20,7 +22,20 @@ function lost(){
     for (var i = 0; i < boundary.length-1; i++) {
         boundary[i].className += " youlose";
         gameStart = true;
+        document.getElementById("status").innerHTML="You Lose!";
         }
     }
 }
  
+function endgame(){
+    if (!gameStart)
+        {
+           document.getElementById("status").innerHTML="You Win!";
+        }
+    else
+        {
+            document.getElementById("status").innerHTML="You Lose!";
+        }
+}
+
+
