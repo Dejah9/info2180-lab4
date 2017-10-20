1841ae6ac921385f4874e68ccf4a1f9c7c78719a
Dejah9 <khadejahhylton@yahoo.com>
2017-10-20 15:11:33 -0500

    Ex2 complete-  All boundaries turn red

--- maze.js
+++ maze.js
@@ -1,10 +1,26 @@
-//Exercise 1 
+ 
+    var gameStart = false;
+    var lose = false;
+
+//Exercise 2
 window.onload = function(){
-    var boundary = document.getElementById("boundary1");
-    boundary.onmouseover= function(){
-        if(boundary.className !== "boundary youlose"){
-            boundary.className += " youlose";
-        }
+    var boundary = document.querySelectorAll(".boundary");
+
+
+    for (var i=0; i< boundary.length-1; i++){
+        boundary[i].addEventListener("mouseover", lost);
 
     }
 }
+    
+
+function lost(){
+    if (!gameStart){
+        var boundary = document.querySelectorAll(".boundary");
+    for (var i = 0; i < boundary.length-1; i++) {
+        boundary[i].className += " youlose";
+        gameStart = true;
+        }
+    }
+}
+ 
